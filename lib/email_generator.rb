ee72965533be146d6706bf176fd79571ae0e3f5e
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-29 13:23:41 +0100

    Added code to fix tests

--- lib/email_generator.rb
+++ lib/email_generator.rb
@@ -1,6 +1,10 @@
 class EmailGenerator
-  def first_name_dot_last_name(name, domain)
-    email = name.downcase.split(" ").join(".")
-    email += "@#{domain}"
+  def initialize(name, domain)
+    @name, @domain = name, domain
+  end
+
+  def first_name_dot_last_name
+    email = @name.downcase.split(" ").join(".")
+    email += "@#{@domain}"
   end
 end
