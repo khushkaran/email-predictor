5adde13ee787dc34585bd651d8758643d7963373
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-29 13:21:22 +0100

    Refactored tests

--- spec/email_generator_spec.rb
+++ spec/email_generator_spec.rb
@@ -2,10 +2,12 @@ require "spec_helper"
 require "email_generator"
 
 describe EmailGenerator do
-  context "returning emails" do
+  context "can return an email" do
+    before{
+      @emailgen = EmailGenerator.new
+    }
     specify "with the pattern first_name_dot_last_name" do
-      emailgen = EmailGenerator.new
-      actual_pattern = emailgen.first_name_dot_last_name("Khushkaran Bajwa", "kesrinishaan.com")
+      actual_pattern = @emailgen.first_name_dot_last_name("Khushkaran Bajwa", "kesrinishaan.com")
       expected_pattern = "khushkaran.bajwa@kesrinishaan.com"
       expect(actual_pattern).to eq expected_pattern
     end
