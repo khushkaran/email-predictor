6d3ebb2293d6eb10b664a937729954ff66db98f6
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-29 14:16:47 +0100

    Added code to return pattern 3

--- lib/email_generator.rb
+++ lib/email_generator.rb
@@ -17,6 +17,10 @@ class EmailGenerator
   end
 
   def first_initial_dot_last_name
-    
+    email = @name.downcase.split(" ")
+    first_name = email.delete_at(0)
+    email.insert(0, first_name[0])
+    email = email.join(".")
+    email += "@#{@domain}"
   end
 end
