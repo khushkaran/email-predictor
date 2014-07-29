50e0e6940988df8701b34bae3f54326ab9a0cbff
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-29 10:31:33 +0100

    Added test for current dataset

--- spec/predictor_spec.rb
+++ spec/predictor_spec.rb
@@ -1,1 +1,10 @@
 require "spec_helper"
+require "predictor"
+
+describe Predictor do
+  it "knows the current dataset" do
+    predictor = Predictor.new
+    expected_dataset = {"John Ferguson" => "john.ferguson@alphasights.com","Damon Aw" => "damon.aw@alphasights.com","Linda Li" => "linda.li@alphasights.com","Larry Page" => "larry.p@google.com","Sergey Brin" => "s.brin@google.com","Steve Jobs" => "s.j@apple.com"}
+    expect(predictor.dataset).to eq expected_dataset
+  end
+end
