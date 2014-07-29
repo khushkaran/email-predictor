0d81370a646ccca9342d9f1c26c04d02d45eac42
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-29 10:32:56 +0100

    Dataset method returns the current dataset

--- lib/predictor.rb
+++ lib/predictor.rb
@@ -1,4 +1,12 @@
 class Predictor
   def dataset
+    {
+      "John Ferguson" => "john.ferguson@alphasights.com",
+      "Damon Aw" => "damon.aw@alphasights.com",
+      "Linda Li" => "linda.li@alphasights.com",
+      "Larry Page" => "larry.p@google.com",
+      "Sergey Brin" => "s.brin@google.com",
+      "Steve Jobs" => "s.j@apple.com"
+    }
   end
 end
