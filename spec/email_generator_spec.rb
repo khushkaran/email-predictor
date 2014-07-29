4cdbdcd73f822dfdd897326d58e98f515cc11bb5
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-29 13:14:28 +0100

    Added test for first pattern

--- spec/email_generator_spec.rb
+++ spec/email_generator_spec.rb
@@ -2,4 +2,12 @@ require "spec_helper"
 require "email_generator"
 
 describe EmailGenerator do
+  context "returning emails" do
+    specify "with the pattern first_name_dot_last_name" do
+      emailgen = EmailGenerator.new
+      actual_pattern = emailgen.first_name_dot_last_name("Khushkaran Bajwa", "kesrinishaan.com")
+      expected_pattern = "khushkaran.bajwa@kesrinishaan.com"
+      expect(actual_pattern).to eq expected_pattern
+    end
+  end
 end
