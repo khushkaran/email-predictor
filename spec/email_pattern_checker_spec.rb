14ec5061d972997ffa002d980d615dd5b1c4d801
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-30 19:46:40 +0100

    Refactored tests and added test for pattern of email string

--- spec/email_pattern_checker_spec.rb
+++ spec/email_pattern_checker_spec.rb
@@ -2,8 +2,15 @@ require "spec_helper"
 require "email_pattern_checker"
 
 describe EmailPatternChecker do
+  before{
+    @checker = EmailPatternChecker.new("john.ferguson@alphasights.com")
+  }
+
   it "can be initialised with an email string" do
-    checker = EmailPatternChecker.new("john.ferguson@alphasights.com")
-    expect(checker.email).to eq "john.ferguson@alphasights.com"
+    expect(@checker.email).to eq "john.ferguson@alphasights.com"
+  end
+
+  it "knows the patter of the initialised email string" do
+    expect(@checker.pattern).to eq "first_name_dot_last_initial"
   end
 end
