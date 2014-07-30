452b777bef1719e165800c37a9b9f7c02623c93b
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-30 19:58:41 +0100

    Fixed broken spec

--- spec/email_pattern_checker_spec.rb
+++ spec/email_pattern_checker_spec.rb
@@ -12,6 +12,6 @@ describe EmailPatternChecker do
   end
 
   it "knows the patter of the initialised email string" do
-    expect(@checker.pattern).to eq "first_name_dot_last_initial"
+    expect(@checker.pattern).to eq "first_name_dot_last_name"
   end
 end
