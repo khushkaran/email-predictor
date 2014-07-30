7ba81a454ce6a3a176e970f51c4f409acc8c0ed4
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-30 19:15:16 +0100

    Added test for initializing the predictor with a name and domain

--- spec/predictor_spec.rb
+++ spec/predictor_spec.rb
@@ -7,4 +7,10 @@ describe Predictor do
     expected_dataset = {"John Ferguson" => "john.ferguson@alphasights.com","Damon Aw" => "damon.aw@alphasights.com","Linda Li" => "linda.li@alphasights.com","Larry Page" => "larry.p@google.com","Sergey Brin" => "s.brin@google.com","Steve Jobs" => "s.j@apple.com"}
     expect(predictor.dataset).to eq expected_dataset
   end
+
+  it "can initialised with name and domain strings" do
+    predictor = Predictor.new("Peter Wong", "alphasights.com")
+    expect(predictor.name).to eq "Peter Wong"
+    expect(predictor.domain).to eq "alphasights.com"
+  end
 end
