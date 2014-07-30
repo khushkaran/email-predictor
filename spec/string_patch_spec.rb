5ed7443480c478a76d5ab5fe2efa92847796947b
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-30 18:55:37 +0100

    Added test for extracting domain from email string

--- spec/string_patch_spec.rb
+++ spec/string_patch_spec.rb
@@ -2,5 +2,8 @@ require "spec_helper"
 require "string_patch"
 
 describe String do
-  
+  it "can extract the domain name from an email string" do
+    actual_domain = "john.ferguson@alphasights.com".to_domain
+    expect(actual_domain).to eq "alphasights.com"
+  end
 end
