0e2752f30bfa871778422ef0c690753d885cd552
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-30 19:31:08 +0100

    Refactored tests

--- spec/predictor_spec.rb
+++ spec/predictor_spec.rb
@@ -8,15 +8,20 @@ describe Predictor do
     expect(predictor.dataset).to eq expected_dataset
   end
 
-  it "can initialised with name and domain strings" do
-    predictor = Predictor.new("Peter Wong", "alphasights.com")
-    expect(predictor.name).to eq "Peter Wong"
-    expect(predictor.domain).to eq "alphasights.com"
-  end
+  context "initialised with name and domain strings" do
+    before{
+      @predictor = Predictor.new("Peter Wong", "alphasights.com")
+    }
+    
+    it "can initialised with name and domain strings" do
+      expect(@predictor.name).to eq "Peter Wong"
+      expect(@predictor.domain).to eq "alphasights.com"
+    end
 
-  it "can get the record(s) for the it domain" do
-    predictor = Predictor.new("Peter Wong", "alphasights.com")
-    expected_matches = {"John Ferguson" => "john.ferguson@alphasights.com","Damon Aw" => "damon.aw@alphasights.com","Linda Li" => "linda.li@alphasights.com"}
-    expect(predictor.matches).to eq expected_matches
+    it "can get the record(s) for the it domain" do
+      predictor = Predictor.new("Peter Wong", "alphasights.com")
+      expected_matches = {"John Ferguson" => "john.ferguson@alphasights.com","Damon Aw" => "damon.aw@alphasights.com","Linda Li" => "linda.li@alphasights.com"}
+      expect(@predictor.matches).to eq expected_matches
+    end
   end
 end
