49e52147e0c929c9ece8aaafdef686593ca07304
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-30 19:43:54 +0100

    changed wording of spec

--- spec/email_pattern_checker_spec.rb
+++ spec/email_pattern_checker_spec.rb
@@ -4,6 +4,6 @@ require "email_pattern_checker"
 describe EmailPatternChecker do
   it "can be initialised with an email string" do
     checker = EmailPatternChecker.new("john.ferguson@alphasights.com")
-    expect(checker.email).to be "john.ferguson@alphasights.com"
+    expect(checker.email).to eq "john.ferguson@alphasights.com"
   end
 end
