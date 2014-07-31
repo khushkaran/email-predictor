298bc724b1b37937df29151665472ed0fd332b06
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-31 18:31:27 +0100

    Added test for email_predictor_feature

--- spec/features/email_predictor_feature_spec.rb
+++ spec/features/email_predictor_feature_spec.rb
@@ -1,5 +1,11 @@
 require "spec_helper"
 
 describe "Web Interface" do
-  
+  it "can return the correct email" do
+    visit '/'
+    fill_in "advisor_name", with: "Craig Silverstein"
+    fill_in "advisor_domain", with: "google.com"
+    click_button 'Submit'
+    expect(page).to have_content "craig.s@google.com"
+  end
 end
