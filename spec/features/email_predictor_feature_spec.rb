cf10d21c1cb0aef5be1ed4fcb523da803ae4d76c
Khushkaran Singh Bajwa <khushkaran@me.com>
2014-07-31 18:53:29 +0100

    Added test for starting a new query

--- spec/features/email_predictor_feature_spec.rb
+++ spec/features/email_predictor_feature_spec.rb
@@ -1,11 +1,19 @@
 require "spec_helper"
 
 describe "Web Interface" do
-  it "can return the correct email" do
+  before{
     visit '/'
     fill_in "advisor_name", with: "Craig Silverstein"
     fill_in "advisor_domain", with: "google.com"
     click_button 'Submit'
+  }
+
+  it "can return the correct email" do
     expect(page).to have_content "craig.s@google.com"
+  end
+
+  it "can clear the current query" do
+    click_button 'New Query'
+    expect(page).not_to have_content "craig.s@google.com"
   end
 end
